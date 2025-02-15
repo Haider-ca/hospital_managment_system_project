09e540c90742c6a207783f32b4b00b10edcdac96
Jbalfour5 <jbalfour5@my.bcit.ca>
2025-02-15 12:50:42 -0800

    Initial Commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/haide/CLionProjects/hospital_managment_system_project
+# Install script for directory: C:/Users/jpvba/CLionProjects/hospital_managment_system_project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/haide/CLionProjects/hospital_managment_system_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/jpvba/CLionProjects/hospital_managment_system_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
